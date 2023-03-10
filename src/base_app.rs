cfc03535e45e999a6ea716c7b11215081a31d4b9
Desktop\Damian <dtrebilco@gmail.com>
2023-03-10 22:39:05 +1000

    Base app update

--- src/base_app.rs
+++ src/base_app.rs
@@ -1,22 +1,24 @@
 use crate::vector::*;
 
-struct BaseApp {
-    app_time: f32,
-    frame_time: f32,
+pub struct BaseApp {
+    pub app_time: f32,
+    pub frame_time: f32,
     start_ticks: u64,
     time_ticks: u64,
 
-    cam_pos: vec3,
-    wx: f32,
-    wy: f32,
-    wz: f32,
+    pub cam_pos: vec3,
+    pub wx: f32,
+    pub wy: f32,
+    pub wz: f32,
 
-    key_left: bool,
-    key_right: bool,
-    key_down: bool,
-    key_up: bool,
-    key_backward: bool,
-    key_forward: bool,
+    pub key_left: bool,
+    pub key_right: bool,
+    pub key_down: bool,
+    pub key_up: bool,
+    pub key_backward: bool,
+    pub key_forward: bool,
+
+    app : dyn App,
 }
 
 pub trait App {
