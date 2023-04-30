3f3b767760f8850f4a8a7fdec3d0c092b1b06a39
Desktop\Damian <dtrebilco@gmail.com>
2023-04-30 23:10:16 +1000

    Add light impl

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,134 @@ use sapp::*;
 use timer::*;
 use vector::*;
 
+struct Light {
+    particles : ParticleSystem,
+    position : vec3,
+    radius : f32,
+    xs :f32,
+    ys :f32,
+    zs :f32,
+}
+
+impl Light {
+
+    fn new(position :vec3, radius : f32, xs : f32, ys : f32, zs: f32) -> Light
+    {
+        let mut new_light = Light {
+            particles: ParticleSystem::new(),
+            position,
+            radius,
+            xs,
+            ys,
+            zs,
+        };
+
+        new_light.particles.spawn_rate = 400.0;
+        new_light.particles.speed = 70.0;
+        new_light.particles.speed_spread = 20.0;
+        new_light.particles.life = 3.0;
+        new_light.particles.life_spread = 0.0;
+        new_light.particles.directional_force = vec3(0.0, -10.0, 0.0);
+        new_light.particles.friction_factor = 0.95;
+        //particles.setPosition(pos);
+        new_light.particles.size = 15.0;
+        new_light.particles.size_spread = 5.0;
+    
+        for i in 0..6 {
+          let i_f = i as f32;  
+          new_light.particles.colors[i] = vec4(0.05 * i_f, 0.01 * i_f, 0.0, 0.0);
+          new_light.particles.colors[i+6] = vec4(0.05 * 6.0, 0.05 * i_f + 0.06, 0.02 * i_f, 0.0);
+        }
+
+        new_light
+    }
+
+    fn calc_light_offset(&self, t : f32, j : f32) -> vec3 
+    {
+        vec3(self.xs * f32::cos(4.23 * t + j), self.ys * f32::sin(2.37 * t) * f32::cos(1.39 * t), self.zs * f32::sin(3.12 * t + j))
+    }
+
+}
+
+  /*
+  struct Portal {
+    inline Portal(uint32_t sect, const vec3& vc0, const vec3& vc1, const vec3& vc2) {
+      sector = sect;
+      v[0] = vc0;
+      v[1] = vc1;
+      v[2] = vc1 + vc2 - vc0;
+      v[3] = vc2;
+    }
+  
+    vec3 v[4];
+    uint32_t sector = 0;
+  };
+  
+  class Sector {
+  public:
+  
+    inline bool isInBoundingBox(vec3& pos) const {
+      return (pos.x > min.x && pos.x < max.x&&
+        pos.y > min.y && pos.y < max.y&&
+        pos.z > min.z && pos.z < max.z);
+    }
+  
+    inline bool isSphereInSector(const vec3& pos, const float radius) const {
+      return (getDistanceSqr(pos) < radius * radius);
+    }
+  
+    inline float getDistanceSqr(const vec3& pos) const {
+      float s, d = 0;
+      for (int i = 0; i < 3; i++) {
+        if (pos[i] < min[i]) {
+          s = pos[i] - min[i];
+          d += s * s;
+        }
+        else if (pos[i] > max[i]) {
+          s = pos[i] - max[i];
+          d += s * s;
+        }
+      }
+      return d;
+    }
+  
+  
+    Model room;
+    std::vector<Portal> portals;
+    std::vector<Light> lights;
+  
+    vec3 min, max;
+    bool hasBeenDrawn = false;
+  };
+  
+  class App : public BaseApp
+  {
+  public:
+  
+    void ResetCamera() override;
+    bool Load() override;
+    void DrawFrame() override;
+  
+  protected:
+  
+    Sector sectors[5];
+  
+    sg_shader shader = {};
+    sg_image base[3] = {};
+    sg_image bump[3] = {};
+    sg_pipeline room_pipline = {};
+    sg_pipeline room_pipline_blend = {};
+  
+    sg_shader pfx_shader = {};
+    sg_image pfx_particle = {};
+    sg_pipeline pfx_pipline = {};
+  
+    sg_buffer pfx_index = {};
+    sg_buffer pfx_vertex = {};
+  
+  };
+*/
+
 struct App {
     timer: Timer,
 }
